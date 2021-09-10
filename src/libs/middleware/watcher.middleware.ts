2f589d8805b365ee5b3f5fa049ddad0fa98a145e
Rafsun Jany Arman <armanrafsunjany@gmail.com>
2021-09-10 10:19:50 +0600

    feat: add 404 and non-func without db

--- src/libs/middleware/watcher.middleware.ts
+++ src/libs/middleware/watcher.middleware.ts
@@ -7,15 +7,15 @@ export const WatcherMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
-  // const url: string = req.protocol + '://' + req.get('host') + req.originalUrl;
-  // const permittedExt = ['css', 'js', 'ico', 'svg'];
-  // const permittedURL = ['/', '/url_shortener'];
+  const url: string = req.protocol + '://' + req.get('host') + req.originalUrl;
+  const permittedExt = ['css', 'js', 'ico', 'svg'];
+  const permittedURL = ['/', '/url_shortener'];
 
-  // console.log(url.split('.').slice(-1)[0], permittedExt)
-  // if (
-  //   permittedExt.indexOf(url.split('.').slice(-1)[0]) >= 0 ||
-  //   permittedURL.indexOf(req.originalUrl) >= 0
-  // )
-  next();
-  // else FetchData(req, res, _404);
+  console.log(url.split('.').slice(-1)[0], permittedExt);
+  if (
+    permittedExt.indexOf(url.split('.').slice(-1)[0]) >= 0 ||
+    permittedURL.indexOf(req.originalUrl) >= 0
+  )
+    next();
+  else FetchData(req, res, _404);
 };
